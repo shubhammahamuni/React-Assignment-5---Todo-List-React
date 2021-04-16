6875b8cd8b50524e757a77fcbf7a729ec0bb6413
shubhammahamuni <shubham.mahamuni.902@gmail.com>
2021-04-16 14:34:29 +0530

    div with class name editingareaa has been deleted

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -19,9 +19,9 @@ function InputTask({list , delete_todo_item,edit_todo_item,handle_edit_save_butt
                    return (<> <li key ={i} className="list">{obj.task_name}</li>
                  <button className="delete"  onClick={()=> {delete_todo_item(obj.task_name ,i)}}>X</button>
                  <button className="edit"  onClick={()=> {edit_todo_item(obj.task_name,i)}}>edit</button>
-                 {  obj.edit==true ?<div className="editingarea"><textarea className="editTask" defaultValue={obj.task_name}  onChange={(e)=>handle_edit(e.target.value , i)}/>
+                 {  obj.edit==true ?<><textarea className="editTask" defaultValue={obj.task_name}  onChange={(e)=>handle_edit(e.target.value , i)}/>
                         
-                        <button className="saveTask" onClick = {()=>{handle_edit_save_button(i)}}>save</button></div>:null}
+                        <button className="saveTask" onClick = {()=>{handle_edit_save_button(i)}}>save</button></>:null}
 
                    </>) })
            
